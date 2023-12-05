865968c48219fc77b74beed8e9700f86779b4b9c
Anastasia Kotelnikova <96003382+Anastasia-kot@users.noreply.github.com>
2023-12-05 08:23:22 +0300

    Create starCoffee.tsx

---
+++ starCoffee.tsx
@@ -0,0 +1,39 @@
+abstract class Beverage {
+    description: string = "Unknown Beverage";
+
+    public getDescription(): string {
+        return this.description;
+    }
+    public abstract cost(): number;
+}
+
+abstract class CondimentDecorator extends Beverage {
+    public abstract getDescription(): string;
+}
+
+class Espresso extends Beverage {
+
+    public Espresso() {
+        this.description = "Espresso";
+    }
+
+    public cost() {
+        return 1.99;
+    }
+}
+
+class Whip extends CondimentDecorator {
+    beverage: Beverage;
+
+    public Whip(beverage: Beverage) {
+        this.beverage = beverage;
+    }
+
+    public getDescription() {
+        return this.beverage.getDescription() + "+ Whip";
+    }
+    
+    public cost() {
+        return .20 + this.beverage.cost();
+    }
+}
